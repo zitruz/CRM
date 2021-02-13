d99b1a14301bbdb3d2090771f68cafa7cbf9b9af
carlos <karlos.diaz54@gmail.com>
2021-02-12 22:49:18 -0600

    port change

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    port: process.env.PORT || 5400,
+    port: process.env.PORT || 5052,
     db: process.env.MONGODB_URI || 'mongodb://localhost:27017/CRM',
     SECRET_TOKEN: '12345essecreto'
   }
